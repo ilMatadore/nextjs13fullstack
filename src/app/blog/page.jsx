3e5e6733a5decf63de6d6c4b13dccd536820b49d
ilMatadore <restarter.dev@gmail.com>
2023-07-11 16:07:34 -0500

    updating urls

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,13 +5,11 @@ import styles from './page.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-
 const Blog = () => {
   const [data ,setData] = useState([])
   useEffect(() => {
     async function getData () {
-      const res = await fetch("http://localhost:3000/api/posts", {
+      const res = await fetch('/api/posts', {
         cache: "no-store",
       });
     
